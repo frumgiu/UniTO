e2e8ba2f9ee84be4ba6a7a5b02614dc9a86490bd
Giulia Frumento <giulia.frumento856@edu.unito.it>
2022-08-09 12:04:05 +0200

    using deck without Vue component. Still not showing data

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,30 +1,20 @@
 <template>
-  <VueDeckgl
-    :layers="layers"
-    :viewState="viewState"
-    @click="handleClick"
-    @view-state-change="updateViewState"
-    class="deck-class">
+  <div class="deck-container">
     <div id="map" ref="map"/>
-  </VueDeckgl>
+    <canvas id="deck-canvas" ref="canvas"/>
+  </div>
 </template>
 
 <script>
-import { ScreenGridLayer } from "@deck.gl/aggregation-layers";
+import { Deck } from "@deck.gl/core";
+import { ScreenGridLayer } from"@deck.gl/aggregation-layers";
 import mapboxgl from "mapbox-gl";
-import VueDeckgl from 'vue-deck.gl'
 
 export default {
-  name: "Map",
-  components: {
-    VueDeckgl
-  },
-  props: ['dataGeo'],
   data() {
     return {
       accessToken: "pk.eyJ1IjoicG9zaWU5OCIsImEiOiJjbDV5MTVteXAwOHRoM2VwZDFlYzN4YTJuIn0.1rRyi4xUKIBqfnhfA9GfVQ",
       mapStyle: "mapbox://styles/posie98/cl5xzs8te001614lidiwsno0m",
-      pathData: this.dataGeo,
       viewState: {
         latitude: 44.3072,
         longitude: 8.484106,
@@ -32,86 +22,99 @@ export default {
         bearing: 0,
         pitch: 0,
       },
-
+      pathData: [
+        {position: [45.054847, 7.686736]},
+        {position: [45.071217, 7.685089]},
+        {position: [44.30459, 8.48426]},
+      ]
     };
   },
   created() {
-    // We need to set mapbox-gl library here in order to use it in template
     this.map = null;
-  },
-  methods: {
-    updateViewState(viewState) {
-      console.log("updating view state...");
-      this.viewState = {
-        ...viewState
-      }
-      this.map.jumpTo({
-        center: [viewState.longitude, viewState.latitude],
-        zoom: viewState.zoom,
-        bearing: viewState.bearing,
-        pitch: viewState.pitch,
-      });
-    },
-
-    handleClick() {
-    }
+    this.deck = null;
   },
   mounted() {
-    // creating the map
     this.map = new mapboxgl.Map({
       accessToken: this.accessToken,
       container: this.$refs.map,
       interactive: false,
-      style:
-          this.mapStyle || "mapbox://styles/posie98/cl5xzs8te001614lidiwsno0m",
+      style: this.mapStyle || "mapbox://styles/posie98/cl5xzs8te001614lidiwsno0m",
       center: [this.viewState.longitude, this.viewState.latitude],
       zoom: this.viewState.zoom,
       pitch: this.viewState.pitch,
       bearing: this.viewState.bearing,
     });
+
+    this.deck = new Deck({
+      canvas: this.$refs.canvas,
+      width: '100%',
+      height: '100%',
+      initialViewState: this.viewState,
+      controller: true,
+      onViewStateChange: ({ viewState }) => {
+        this.map.jumpTo({
+          center: [viewState.longitude, viewState.latitude],
+          zoom: viewState.zoom,
+          bearing: viewState.bearing,
+          pitch: viewState.pitch,
+        });
+      },
+    })
   },
   computed: {
-    layers() {
-      const layer =  new ScreenGridLayer({
-        id: "screen-grid-layer",
-        data: this.dataGeo,
-        visible: true,
+    getLayers() {
+      const layers = new ScreenGridLayer({
+        id: 'test-layer',
+        data: this.pathData,
+        opacity: 1,
         cellSizePixels: 50,
-        opacity: 0.8,
-        colorRange: [
-          [255, 255, 178, 25],
-          [254, 217, 118, 85],
-          [254, 178, 76, 127],
-          [253, 141, 60, 170],
-          [240, 59, 32, 212],
-          [189, 0, 38, 255]
+        getPosition: (d) => d.position,
+        getWeight: 4,
+        colorRange:  [
+          [0, 25, 0, 25],
+          [0, 85, 0, 85],
+          [0, 127, 0, 127],
+          [0, 170, 0, 170],
+          [0, 190, 0, 190],
+          [0, 255, 0, 255]
         ],
         gpuAggregation: true,
-        aggregation: 'SUM',
-        getPosition: d => d.coordinates,
-        getWeight: 4
+        aggregation: 'SUM'
       });
-      return [layer];
+      return [layers];
+    }
+  },
+  watch: {
+    //Watch the computed prop, if data change the layer change
+    getLayers(newLayers) {
+      this.deck.setProps({newLayers});
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .deck-class {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-  #map {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: #e5e9ec;
-    overflow: hidden;
-  }
+.deck-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+#map {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #e5e9ec;
+  overflow: hidden;
+}
+#deck-canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
