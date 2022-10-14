7090905526225ed69dbdf6170c48bdd671ba2c25
Giulia Frumento <giulia.frumento856@edu.unito.it>
2022-10-14 11:37:59 +0200

    add gallery component

---
+++ client/src/components/Gallery.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="gallery-display">
+    <div id="gallery">
+      <hr class="solid" style="margin-top: 5rem"/>
+      <div class="image-group-wrapper" >
+        <div v-for="(value, index) in savedData" :key="index">
+          <GalleryElement :singleDataFileName="value.filename"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GalleryElement from "@/components/GalleryElement";
+
+export default {
+  name: "Gallery",
+  components: {
+    GalleryElement
+  },
+  props: ['savedData']
+}
+</script>
+
+<style scoped>
+  @import url('../resources/stylesheets/gallery/gallery-container.css');
+</style>
