4fbe40e0258c3a6cc539793c59685f5f3d2afe71
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-02-18 10:58:51 +0100

    Docker prende il codice di autenticazione in input (dcoker-compose run demotelegramapi)

--- src/main/java/com/example/demotelegramapi/helpers/SimpleHelper.java
+++ src/main/java/com/example/demotelegramapi/helpers/SimpleHelper.java
@@ -3,17 +3,20 @@ package com.example.demotelegramapi.helpers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class SimpleHelper {
     public static String promptString(String prompt) {
         System.out.print(prompt);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+       /* BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String str = "";
         try {
             str = reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return str;
+        return str; */
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
