5d0661b6fbfa78db2a1b65646cf6aa65e23a505a
Fabio Ferrero <fabio.ferrero@hotmail.it>
2023-02-21 11:19:10 +0100

    Modifica file config yml kubernetes con immagini docker hub

--- orchestration/deploy.sh
+++ orchestration/deploy.sh
@@ -5,6 +5,8 @@ kubectl apply -f db-users.yml
 kubectl apply -f db-contents.yml
 kubectl apply -f db-saleads.yml
 
+sleep 2
+
 kubectl apply -f eureka.yml
 kubectl apply -f api-gateway.yml
 
@@ -12,6 +14,6 @@ kubectl apply -f users-service.yml
 kubectl apply -f contents-service.yml
 kubectl apply -f balon-service.yml
 
-sleep 2
+sleep 3
 
 kubectl apply -f telegram-service.yml
--- orchestration/image-hub.sh
+++ orchestration/image-hub.sh
@@ -1,38 +1,38 @@
 cd ..
 
 cd eureka-server
-docker image build -t fabioferrero/unititaass:service-discovery-server .
-docker image push fabioferrero/unititaass:service-discovery-server
+docker image build -t fabioferrero/unitiprog:service-discovery-server .
+docker image push fabioferrero/unitiprog:service-discovery-server
 
 cd ..
 
 cd api-gateway
-docker image build -t fabioferrero/unititaass:api-gateway .
-docker image push fabioferrero/unititaass:api-gateway
+docker image build -t fabioferrero/unitiprog:api-gateway .
+docker image push fabioferrero/unitiprog:api-gateway
 #
 cd ..
 
 cd user-microservice
-docker image build -t fabioferrero/unititaass:users-service .
-docker image push fabioferrero/unititaass:users-service
+docker image build -t fabioferrero/unitiprog:users-service .
+docker image push fabioferrero/unitiprog:users-service
 
 cd ..
 
 cd content-microservice
-docker image build -t fabioferrero/unititaass:contents-service .
-docker image push fabioferrero/unititaass:contents-service
+docker image build -t fabioferrero/unitiprog:contents-service .
+docker image push fabioferrero/unitiprog:contents-service
 
 cd ..
 
 cd balon-microservice
-docker image build -t fabioferrero/unititaass:balon-service .
-docker image push fabioferrero/unititaass:balon-service
+docker image build -t fabioferrero/unitiprog:balon-service .
+docker image push fabioferrero/unitiprog:balon-service
 
 cd ..
 
 cd telegram-microservice
-docker image build -t fabioferrero/unititaass:telegram-service .
-docker image push fabioferrero/unititaass:telegram-service
+docker image build -t fabioferrero/unitiprog:telegram-service .
+docker image push fabioferrero/unitiprog:telegram-service
 
 
 
