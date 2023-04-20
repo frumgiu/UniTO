7819da78a575f205b8123a5abc26ba2c0b59977e
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-04-20 14:18:33 +0200

    finito

--- EsercizioUno/CorrelationCoeff.py
+++ EsercizioUno/CorrelationCoeff.py
@@ -2,12 +2,11 @@ import numpy as np
 
 
 def pearson_coeff(x, y):
-    covariance = np.mean(x*y) - np.mean(x)*np.mean(y)  # covariance = (np.sum(x*y) / 4) - np.mean(x)*np.mean(y)
-    return covariance / (np.std(x) * np.std(y))
+    covariance = np.mean(x * y) - np.mean(x) * np.mean(y)  # covariance = (np.sum(x*y) / 4) - np.mean(x)*np.mean(y)
+    return round((covariance / (np.std(x) * np.std(y))), 4)
 
 
 def spearman_coeff(x, y):
     x_ranks = np.argsort(np.argsort(x))
     y_ranks = np.argsort(np.argsort(y))
-    return pearson_coeff(x_ranks, y_ranks)
-
+    return round((pearson_coeff(x_ranks, y_ranks)), 4)
--- EsercizioUno/Exercise.py
+++ EsercizioUno/Exercise.py
@@ -2,6 +2,7 @@ import Readcsv as rd
 import numpy as np
 from SimilarityFunctions import similarity_function as sf
 from CorrelationCoeff import pearson_coeff, spearman_coeff
+from scipy.stats import pearsonr, spearmanr
 
 # Read the file
 right_values, left_values, human_sim = rd.read_wordsimcsv()
@@ -14,7 +15,21 @@ result_sp = sf('SP', right_values, left_values)
 # Calculate coefficients
 pc_wp = pearson_coeff(np.array(result_wp), human_sim)
 print('Pearson coeff for WP: ', pc_wp)
+print('Pearson coeff LIB for WP: ', pearsonr(np.array(result_wp), human_sim))
 pc_lc = pearson_coeff(np.array(result_lc), human_sim)
 print('Pearson coeff for LC: ', pc_lc)
+print('Pearson coeff LIB for LC: ', pearsonr(np.array(result_lc), human_sim))
 pc_sp = pearson_coeff(np.array(result_sp), human_sim)
 print('Pearson coeff for SP: ', pc_sp)
+print('Pearson coeff LIB for SP: ', pearsonr(np.array(result_sp), human_sim), '\n\n')
+
+sc_wp = spearman_coeff(np.array(result_wp), human_sim)
+print('Spearman coeff for WP: ', sc_wp)
+print('Spearman coeff LIB for WP: ', spearmanr(np.array(result_wp), human_sim))
+sc_lc = spearman_coeff(np.array(result_lc), human_sim)
+print('Spearman coeff for LC: ', sc_lc)
+print('Spearman coeff LIB for LC: ', spearmanr(np.array(result_lc), human_sim))
+sc_sp = spearman_coeff(np.array(result_sp), human_sim)
+print('Spearman coeff for SP: ', sc_sp)
+print('Spearman coeff LIB for SP: ', spearmanr(np.array(result_sp), human_sim))
+
