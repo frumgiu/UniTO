136eb5d07439ad1b7ce3600d0d604a48f8e662a5
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-04-27 15:08:11 +0200

    Finito WSD

--- EsercizioDue/LeskAlgorithm.py
+++ EsercizioDue/LeskAlgorithm.py
@@ -72,18 +72,14 @@ def run_exercise(num_test, sentences, sentences_tag):
     final_tot = 0
     final_correct = 0
     for j in range(0, num_test - 1):
-        tot = 0
-        correct = 0
         for i in range(len(sentences)):
-            tot += 1
             final_tot += 1
             word, next_word = get_random_word(sentences_tag[i])
             if '-' in word.word:
-                r = lesk_compose_word(word.word, next_word.word, sentences[i])
+                next_word_temp = next_word if next_word == '' else next_word.word
+                r = lesk_compose_word(word.word, next_word_temp, sentences[i])
             else:
                 r, o = my_lesk(word.word, sentences[i])
             if r is not None and r.name() == word.tag:
-                correct += 1
                 final_correct += 1
-
     return round(final_correct/final_tot, 2)
