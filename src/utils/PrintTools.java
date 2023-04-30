db4a9bb33a611cf463cfb4f15ba8e57a5a59ad30
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-04-30 12:07:25 +0200

    Piccola modifica sulla stampa degli alberi

--- src/utils/PrintTools.java
+++ src/utils/PrintTools.java
@@ -8,11 +8,11 @@ public class PrintTools {
     public static void printResultMatrix(int n, String startSymbol, ArrayList<String> line, HashMap<Integer, HashMap<Integer, ArrayList<Casella>>> table) {
         System.out.print("RESULT: ");
         if (table.get(0) != null && table.get(0).get(n) != null && table.get(0).get(n).size() != 0 && table.get(0).get(n).stream().anyMatch(s -> s.getElement().equals(startSymbol)))
-            System.out.println("The line IS an element of the Context Free Grammar defined");
+            System.out.println("The line IS an element of the Context Free Grammar defined\n");
         else
-            System.out.println("The line IS NOT an element of the Context Free Grammar defined");
+            System.out.println("The line IS NOT an element of the Context Free Grammar defined\n");
 
-        System.out.println("Table created from the CYK algorithm");
+        System.out.println("Table created from the CYK algorithm:\n");
         int numSpace = 50;
         int numSpaceLine = findLongestWord(line);
         for (int j = 0; j < n; j++) {
@@ -38,14 +38,16 @@ public class PrintTools {
 
     public static void printTree(int n, String startSymbol, HashMap<Integer, HashMap<Integer, ArrayList<Casella>>> result) {
         ArrayList<Casella> leaves = result.get(0).get(n);   // prendi le foglie dell'albero
-        System.out.println("Albero/i parsificazione:");
-        for (Casella leaf : leaves) {
-            if (leaf.getElement().equals(startSymbol)) {
-                printNode(leaf, 0);                   // stampa la foglia
-                System.out.println("\n----------------------------\n");
+        if (leaves != null) {                               // se non rispetta la grammatica non stampo niente
+            System.out.println("Trees:\n");
+            for (Casella leaf : leaves) {
+                if (leaf.getElement().equals(startSymbol)) {
+                    printNode(leaf, 0);                   // stampa la foglia
+                    System.out.println("\n----------------------------\n");
+                }
             }
+            System.out.print("\n\n");
         }
-        System.out.print("\n\n");
     }
 
     private static void printNode(Casella node, int depth) {
