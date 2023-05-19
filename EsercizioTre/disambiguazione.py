02392a6ac7780b260d76fe37d25753abdc74d476
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-05-19 20:18:07 +0200

    Esercizio framenet - score finale

--- EsercizioTre/disambiguazione.py
+++ EsercizioTre/disambiguazione.py
@@ -1,6 +1,8 @@
 import string
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
+from nltk.corpus import framenet as fn
+from annotazioneTarget import bail_target, partitive_target, receiving_target, speed_target, mining_target
 
 STOPWORDS = set(stopwords.words('english'))
 PUNCTUATION = string.punctuation
@@ -34,13 +36,10 @@ def mapping_framename(frame, result):
         if temp_score > max_score:
             max_score = temp_score
             best_sense = s
-    # print('\n____ Frame Name disambiguato ____')
-    result[frame.name] = best_sense
-    # print(best_sense, ' ', max_score)
+    result[frame.name] = best_sense.name() if best_sense is not None else None
 
 
 def mapping_fe(frame, result):
-    # print('\n____ FE disambiguati ____')
     for e in frame.FE.keys():
         context_frame = preparation(frame.FE[e].definition)
         best_sense = None
@@ -51,12 +50,10 @@ def mapping_fe(frame, result):
             if temp_score > max_score:
                 max_score = temp_score
                 best_sense = s
-        result[e] = best_sense
-        # print(e, ' ', best_sense, ' ', max_score)
+        result[e] = best_sense.name() if best_sense is not None else None
 
 
 def mapping_lu(frame, result):
-    # print('\n____ LU disambiguati ____')
     for e in frame.lexUnit.keys():
         lu_name = e.split(".")[0]
         context_frame = preparation(frame.lexUnit[e].definition)
@@ -69,13 +66,23 @@ def mapping_lu(frame, result):
                 if temp_score > max_score:
                     max_score = temp_score
                     best_sense = s
-        result[e] = best_sense
-        # print(e, ' ', best_sense, ' ', max_score)
+        result[e] = best_sense.name() if best_sense is not None else None
 
 
-def mapping(frame):
+def mapping_frame(frame):
     result = {}
     mapping_framename(frame, result)
     mapping_fe(frame, result)
     mapping_lu(frame, result)
     return result
+
+
+def mapping():
+    result_one = mapping_frame(fn.frame(1010)).items() & partitive_target.items()
+    result_two = mapping_frame(fn.frame(966)).items() & speed_target.items()
+    result_three = mapping_frame(fn.frame(2285)).items() & mining_target.items()
+    result_four = mapping_frame(fn.frame(411)).items() & receiving_target.items()
+    result_five = mapping_frame(fn.frame(121)).items() & bail_target.items()
+    correct = len(result_one) + len(result_two) + len(result_three) + len(result_four) + len(result_five)
+    total = len(partitive_target) + len(speed_target) + len(mining_target) + len(receiving_target) + len(bail_target)
+    return correct/total
