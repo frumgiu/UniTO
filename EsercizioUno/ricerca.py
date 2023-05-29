3b9222d2c365d7eb836cf7d6631b9e7c4cd9d9ee
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-05-29 21:26:20 +0200

    Esercizio 2 - devo mettere treshold, filtrare i sensi, ...

--- EsercizioUno/ricerca.py
+++ EsercizioUno/ricerca.py
@@ -1,27 +1,33 @@
 from nltk.corpus import wordnet
-from def_overlap_funcs import similarity_score
+from nltk.tokenize import word_tokenize
+from def_overlap_funcs import filter_words
 
 
-def get_genus(target, r):
-    synsets = wordnet.synsets(r[target])
+def get_genus(term):
+    synsets = wordnet.synsets(term, pos=wordnet.NOUN)
     if not synsets:
         return []
-    return synsets[0]  # Prendi il primo synset come target
+    return synsets
 
 
-def get_hyponyms(synset):
-    hyponyms = synset.hyponyms()
-    return hyponyms
+def create_context(h):
+    word_list = []
+    definition_words = word_tokenize(h.definition())
+    example_words = [word for example in h.examples() for word in word_tokenize(example)]
+    word_list.extend(definition_words)
+    word_list.extend(example_words)
+    return filter_words(word_list)
 
 
 def get_ordered_hyponyms(term, definitions):
-    synsets = wordnet.synsets(term, pos=wordnet.NOUN)
+    synsets = get_genus(term)
     hyponyms = []
 
     for synset in synsets:
+        print(synset, synset.max_depth())
         for hyponym in synset.hyponyms():
-            hyponym_words = hyponym.definition().split()
-            similarity = len(set(hyponym_words) & set(definitions))
+            word_list = create_context(hyponym)
+            similarity = len(set(word_list) & set(definitions))
             hyponyms.append((hyponym, similarity))
 
     hyponyms.sort(key=lambda x: x[1], reverse=True)
