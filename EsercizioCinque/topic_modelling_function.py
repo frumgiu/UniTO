b9738e7f33bc6230b175904be3ca12228fab1b11
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-06-01 12:22:34 +0200

    Esercizio 5 - Seguito tutorial online, scegliere altro dataset e sistemare grafico

---
+++ EsercizioCinque/topic_modelling_function.py
@@ -0,0 +1,73 @@
+import re
+import tarfile
+import smart_open
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+# Compute bigrams.
+from gensim.models import Phrases
+# Train LDA model.
+from gensim.models import LdaModel
+
+
+def extract_documents(url='https://cs.nyu.edu/~roweis/data/nips12raw_str602.tgz'):
+    with smart_open.open(url, "rb") as file:
+        with tarfile.open(fileobj=file) as tar:
+            for member in tar.getmembers():
+                if member.isfile() and re.search(r'nipstxt/nips\d+/\d+\.txt', member.name):
+                    member_bytes = tar.extractfile(member).read()
+                    yield member_bytes.decode('utf-8', errors='replace')
+
+
+def pre_processing(docs):
+    lemmatizer = WordNetLemmatizer()
+    tokenizer = RegexpTokenizer(r'\w+')
+    for idx in range(len(docs)):
+        docs[idx] = docs[idx].lower()  # Convert to lowercase.
+        docs[idx] = tokenizer.tokenize(docs[idx])  # Split into words.
+    # Remove numbers, but not words that contain numbers.
+    docs = [[token for token in doc if not token.isnumeric()] for doc in docs]
+    # Remove words that are only one character.
+    docs = [[token for token in doc if len(token) > 1] for doc in docs]
+    # Lemmatizzazione
+    docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+    return docs
+
+
+def find_bigrams(docs):
+    # Add bigrams and trigrams to docs (only ones that appear 20 times or more).
+    bigram = Phrases(docs, min_count=20)
+    for idx in range(len(docs)):
+        for token in bigram[docs[idx]]:
+            if '_' in token:
+                # Token is a bigram, add to document.
+                docs[idx].append(token)
+    return docs
+
+
+def training(corpus, dictionary, num_topics=10, chunksize=200, passes=20, iterations=400, eval_every=None):
+    # Make an index to word dictionary.
+    temp = dictionary[0]  # This is only to "load" the dictionary.
+    id2word = dictionary.id2token
+
+    model = LdaModel(
+        corpus=corpus,
+        id2word=id2word,
+        chunksize=chunksize,
+        alpha='auto',
+        eta='auto',
+        iterations=iterations,
+        num_topics=num_topics,
+        passes=passes,
+        eval_every=eval_every
+    )
+
+    top_topics = model.top_topics(corpus)
+
+    # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+    print('Average topic coherence: %.4f.' % avg_topic_coherence)
+    return top_topics
+
+
+def grafico(topics):
+    return
