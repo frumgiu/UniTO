5211a4785935fcb0be3501e79ec16056127136b5
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-06-06 21:08:02 +0200

    Esercizio 4a - funziona, cambiare corpus.txt

--- EsercizioQuattro/fz_text_seg.py
+++ EsercizioQuattro/fz_text_seg.py
@@ -6,66 +6,71 @@ from nltk.stem import PorterStemmer
 
 
 def tokenization(file_path, w):
-    def create_token_seq(text_, w_):
-        tokens = word_tokenize(text_)
+    def _clean_tokens(tokens):
+        return [token.lower() for token in tokens if token.isalpha()]
+
+    def _remove_stop_word(words):
+        return [word for word in words if word not in stopwords.words('english')]
+
+    def _filter_words(words):
+        return _remove_stop_word(_clean_tokens(words))
+
+    def _create_token_seq(text_, w_):
+        tokens = _clean_tokens(word_tokenize(text_))
         sequences = []
-        for i in range(0, len(tokens) - w_ + 1, w):
+        for i in range(0, len(tokens) - w_ + 1, w_):
             sequences.append(tokens[i:i + w_])
         return sequences
 
-    def find_paragraph_breaks(text_):
-        paragraph_breaks = []
-        for i in range(len(text_)):
-            if text_[i:i + 2] == '\n\n':
-                paragraph_breaks.append(i)
-        return paragraph_breaks
-
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
-
-    tokens_seq = create_token_seq(text, w)
     stemmer = PorterStemmer()
-    unique_words = [word.lower() for word in word_tokenize(text) if word not in stopwords.words('english')]
-    unique_words = list(set(stemmer.stem(word) for word in unique_words))
+    # Sequenze di token
+    tokens_seq = _create_token_seq(text, w)
+    tokens_seq_clean = [_remove_stop_word(ts) for ts in tokens_seq]
+    tokens_seq_stem = [[stemmer.stem(w) for w in token_seq_clean] for token_seq_clean in tokens_seq_clean]
+    # Parole uniche del testo
+    unique_words = list(set(stemmer.stem(word) for word in _filter_words(word_tokenize(text))))
+    return tokens_seq_stem, unique_words
 
-    token_seq_clean = [[w.lower() for w in ts if w not in stopwords.words('english')] for ts in tokens_seq]
-    token_seq_stem = [[stemmer.stem(w) for w in word] for word in token_seq_clean]
-    return token_seq_stem, unique_words, find_paragraph_breaks(text)
+
+def create_blocks(token_seq, k):
+    blocks = []
+    par_tr = []
+    for i in range(0, len(token_seq), k):
+        blocks.append(token_seq[i: i+k])
+        for seq in token_seq[i: i+k]:
+            if 'fabioandrea' in seq:
+                par_tr.append(len(blocks) - 1)
+    return blocks, par_tr
 
 
-def lexical_score(token_seq, word_list, k):
-    def block_score(block_1, block_2, words_list, w_size):
+def lexical_score(blocks_list, word_list):
+    def _blocks_score(block1, block2):
         numerator = 0
-        block1_sum = 0
-        block2_sum = 0
-        for w in words_list:
-            block1_cnt_word = 0
-            block2_cnt_word = 0
-            for i in range(0, w_size):
-                block1_cnt_word = block_1[i].count(w)
-                block2_cnt_word = block_2[i].count(w)
-            numerator += block1_cnt_word * block2_cnt_word
-            block1_sum += block1_cnt_word ** 2
-            block2_sum += block2_cnt_word ** 2
-        return numerator / sqrt(block1_sum * block2_sum)
+        sum_sqrt_1 = 0
+        sum_sqrt_2 = 0
+        for w in word_list:
+            block1_count = 0
+            block2_count = 0
+            for j in range(0, len(block1)):
+                block1_count += block1[j].count(w)
+            for j in range(0, len(block2)):
+                block2_count += block2[j].count(w)
+            numerator += block1_count * block2_count
+            sum_sqrt_1 += block1_count ** 2
+            sum_sqrt_2 += block2_count ** 2
+        return numerator / sqrt(sum_sqrt_1 * sum_sqrt_2)
 
-    ls_blocks_score = []
-    for index in range(k, len(token_seq)):  # da k a numero di sequenze che ho
-        # Devo tenere conto che la finestra non sarà sempre = k, dipende da quante sequenze mi sono rimaste
-        window_size = min(k, len(token_seq) - index)  # index,
-        block1 = token_seq[index - window_size: index]
-        block2 = token_seq[index: index + window_size]
-        ls_blocks_score.append(block_score(block1, block2, word_list, window_size))
-    return ls_blocks_score, np.convolve(ls_blocks_score, np.ones(k) / k, mode='same')   # Smoothing
+    def _smoothing(ls_score, n_smooth=2, w=4):
+        for j in range(0, n_smooth):
+            ls_score = np.convolve(ls_score, np.ones(w) / w, mode='same')
+        return ls_score
 
-
-def depth_cutoff(ls_score, liberal=True):
-    if liberal is True:
-        # Lower precision, higher recall
-        return np.mean(ls_score) - np.std(ls_score)
-    else:
-        # Higher precision, lower recall
-        return np.mean(ls_score) - np.std(ls_score) / 2
+    scores = []
+    for i in range(0, len(blocks_list)-1):
+        scores.append(_blocks_score(blocks_list[i], blocks_list[i+1]))
+    return scores, _smoothing(scores)
 
 
 def depth_side_score(ls_score, gap_i, left):
@@ -83,11 +88,11 @@ def get_depth_score(ls_score):
     depth_scores = []
     for i, score in enumerate(ls_score):
         depthScore = depth_side_score(ls_score, i, True) + depth_side_score(ls_score, i, False)
-        depth_scores.append(depthScore)
+        if i != 0 and i != len(ls_score)-1:
+            depth_scores.append((depthScore, i))
     return depth_scores
 
 
 def find_boundaries(ls_score, num_cuts):
     depth_score = sorted(get_depth_score(ls_score), reverse=True)
-    return depth_score
-
+    return depth_score[0:num_cuts]
