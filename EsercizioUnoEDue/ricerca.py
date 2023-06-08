f1c858f39ae053a9e542edcbbb6cb487978752fe
Giulia Frumento <giulia.frumento856@edu.unito.it>
2023-06-08 15:58:49 +0200

    Esercizio 2 - Visitare tutto l'albero. Al secondo livello di iponimi tornano i risultati giusti. Criterio di arresto più generico?

--- EsercizioUnoEDue/ricerca.py
+++ EsercizioUnoEDue/ricerca.py
@@ -17,23 +17,51 @@ def create_context(h):
     return filter_words(word_list)
 
 
-def get_ordered_words(terms, definitions):
-    def _find_similarity(def_list, h, h_list):
-        similarity = len(set(create_context(h)) & set(def_list))
-        if (h.lemmas()[0].name(), similarity) not in h_list:
-            h_list.append((h.lemmas()[0].name(), similarity))
+def find_words(def_list, h, h_list):
+    sim = len(set(create_context(h)) & set(def_list))
+    if (h, sim) not in h_list:
+        h_list.append((h, sim))
 
+
+def get_ordered_words(terms, definitions):
     hyponyms = []
+
     for i in range(len(terms)):
         syn_list = get_genus(terms[i][0])
-
         for synset in syn_list:
             if synset.max_depth() > 6:
-                for hyponym in synset.hyponyms():
-                    _find_similarity(definitions, hyponym, hyponyms)
-                for meronym in synset.part_meronyms():
-                    _find_similarity(definitions, meronym, hyponyms)
+                [find_words(definitions, hyponym, hyponyms) for hyponym in synset.hyponyms()]
+                # [find_words(definitions, meronym, hyponyms) for meronym in synset.part_meronyms()]
+    # Dopo aver provato con tutti i sensi degli iponimi del genus provo a fare i sensi dei degli iponimi
+    for i in range(0, 1):
+        half = hyponyms[0][1]/2
+        for h in hyponyms:
+            [find_words(definitions, hyp, hyponyms) for hyp in h[0].hyponyms() if h[1] > half]
 
     hyponyms.sort(key=lambda x: x[1], reverse=True)
-    return hyponyms
+    return [(h[0].lemmas()[0].name(), h[1]) for h in hyponyms]
 
+
+def get_ordered_words_2(terms, definitions):
+    hyponyms = []
+    queue = []
+    for i in range(len(terms)):
+        syn_list = get_genus(terms[i][0])
+        for synset in syn_list:
+            if synset.max_depth() > 6:
+                for h in synset.hyponyms():
+                    sim = len(set(create_context(h)) & set(definitions))
+                    if (h, sim) not in hyponyms:
+                        hyponyms.append((h, sim))
+                        queue.append((h, sim))
+                    while queue is not []:
+                        pop = queue[0]
+                        if pop[1] > hyponyms[0][1]/2:
+                            for hh in pop[0].hyponyms():
+                                sim_ = len(set(create_context(hh)) & set(definitions))
+                                if (hh, sim) not in hyponyms:
+                                    hyponyms.append((hh, sim_))
+                                    queue.append((hh, sim_))
+
+    hyponyms.sort(key=lambda x: x[1], reverse=True)
+    return [(h[0].lemmas()[0].name(), h[1]) for h in hyponyms]
